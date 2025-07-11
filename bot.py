11f5b7628b91b8b2eada4ca07301a848f9f0961d
Arya <109459398+Lordarya1@users.noreply.github.com>
2025-07-11 04:21:35 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,12 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-
-# گرفتن توکن از محیط (Render)
-TOKEN = os.environ.get("BOT_TOKEN")
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات با موفقیت فعال شد ✅")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("پیام شما دریافت شد!")
+    await update.message.reply_text("سلام! ربات سالم کار می‌کنه ✅")
 
 if __name__ == '__main__':
+    TOKEN = os.environ.get("BOT_TOKEN")
     app = ApplicationBuilder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    print("Bot is running...")
     app.run_polling()
